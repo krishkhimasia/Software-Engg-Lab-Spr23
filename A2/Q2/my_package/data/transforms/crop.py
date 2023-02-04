85370ae87e538b6d9a4b1820f862f00cde0cbbf3
Krish Khimasia <khimasiakrish@gmail.com>
2023-02-04 14:28:55 +0530

    added code for crop

--- A2/Q2/my_package/data/transforms/crop.py
+++ A2/Q2/my_package/data/transforms/crop.py
@@ -13,6 +13,8 @@ class CropImage(object):
             shape: output shape of the crop (h, w)
             crop_type: center crop or random crop. Default: center
         '''
+        self._shape=shape
+        self._crop_type=crop_type
 
     def __call__(self, image):
         '''
@@ -22,5 +24,23 @@ class CropImage(object):
             Returns:
             image (numpy array or PIL image)
         '''
+        w,h=image.size
+        if(self._shape==(h,w)):     #handles the case where no cropping takes place
+            return image
+        else:
+            if self._crop_type=='random':       #random cropping, randrange() generates random position for cropping
+                if(2*self._shape[1]<=w):
+                    l=random.randrange(self._shape[1],w-self._shape[1])
+                else:
+                    l=random.randrange(0,w-self._shape[1])
+                if(2*self._shape[0]<=h):
+                    t=random.randrange(self._shape[0],h-self._shape[0])
+                else:
+                    t=random.randrange(0,h-self._shape[0])
+            else:                               #center cropping, position chose appropriately to crop at center
+                l=int(w/2-self._shape[1]/2)
+                t=int(h/2-self._shape[0]/2)
+        croppedImage=image.crop((l,t,l+self._shape[1],t+self._shape[0]))       #using PIL to crop the image
+        return croppedImage
 
  
