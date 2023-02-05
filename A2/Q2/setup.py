104fcf65684c86e2859a8b5efb5c8eed96e1a8f1
Krish Khimasia <khimasiakrish@gmail.com>
2023-02-05 17:55:38 +0530

    added setup.py

---
+++ A2/Q2/setup.py
@@ -0,0 +1,11 @@
+from distutils import setup
+from setuptools import find_packages
+
+setup(
+    name="my_package",
+    version="0.1",
+    description="Package for SE Lab Assignment 2, Question 2",
+    author="Krish Khimasia",
+    author_email="khimasiakrish@gmail.com",
+    packages=find_packages()
+)
