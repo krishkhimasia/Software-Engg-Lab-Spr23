fc94ba15ad9c2979e7526d7f89450079d7a8a68c
Krish Khimasia <khimasiakrish@gmail.com>
2023-02-05 12:01:37 +0530

    added dataset code

--- A2/Q2/my_package/data/dataset.py
+++ A2/Q2/my_package/data/dataset.py
@@ -16,21 +16,35 @@ class Dataset(object):
             transforms: list of transforms (class instances)
                         For instance, [<class 'RandomCrop'>, <class 'Rotate'>]
         '''
+        self._annotation_file=annotation_file
+        self._transforms=transforms
      
 
     def __len__(self):
         '''
             return the number of data points in the dataset
         '''
+        cnt=0
+        with jsonlines.open(self._annotation_file) as reader:
+            for obj in reader:
+                cnt+=1
+        return cnt
 
     
     def __getann__(self, idx):
         '''
             return the data items for the index idx as an object
         '''
+        cnt=0
+        with jsonlines.open(self._annotation_file) as reader:
+            return list(reader)[idx]
         
 
     def __transformitem__(self, path):
         '''
             return transformed PIL Image object for the image in the given path
         '''
+        img=Image.open(path)
+        for transform in self._transforms:
+            im=transform(im)
+        return im
